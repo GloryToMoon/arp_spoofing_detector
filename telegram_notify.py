f625d4f2ece4bb14ec59d8035239fb879fd22e67
Dead_moon <58658937+GloryToMoon@users.noreply.github.com>
2021-04-01 18:16:21 +0300

    Update telegram_notify.py

--- telegram_notify.py
+++ telegram_notify.py
@@ -1,4 +1,2 @@
-import telebot
-import sys
-bot = telebot.TeleBot(token='<telegram bot token>')
-bot.send_message(chat_id="<channel id>", text=" ".join(sys.argv[1:]))
+import telebot, sys
+telebot.TeleBot(token='<telegram bot token>').send_message(chat_id="<channel id>", text=" ".join(sys.argv[1:]))
